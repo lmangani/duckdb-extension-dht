cce70be80510bae807b2cdf4a314c25383a0812b
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-11-13 17:26:56 +0100

    status struct

--- src/include/dht_extension.hpp
+++ src/include/dht_extension.hpp
@@ -24,4 +24,31 @@ struct DHTAnnouncement {
     int32_t port;
 };
 
+// Status information structure
+struct DhtStatusInfo {
+    std::string version;
+    std::string node_id;
+    std::string uptime;
+    std::string listen_info;
+    int port;
+    int ipv4_nodes;
+    int ipv4_good_nodes;
+    int ipv6_nodes;
+    int ipv6_good_nodes;
+    int storage_entries;
+    int storage_addresses;
+    int ipv4_searches;
+    int ipv4_searches_done;
+    int ipv6_searches;
+    int ipv6_searches_done;
+    int announcements;
+    int blocklist;
+    double traffic_in;
+    std::string traffic_in_rate;
+    double traffic_out;
+    std::string traffic_out_rate;
+};
+
+void ParseDhtStatus(const std::string& status_str, DhtStatusInfo& info);
+
 } // namespace duckdb
